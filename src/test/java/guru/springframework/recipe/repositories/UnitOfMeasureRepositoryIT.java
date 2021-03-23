3da7efc54d179ba07c1a3cc5923678ec9cc68a2c
jeroen.admiraal@alliander.com <jeroen.admiraal@alliander.com>
2021-03-23 14:32:59 +0100

    Add integration test

---
+++ src/test/java/guru/springframework/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,35 @@
+package guru.springframework.recipe.repositories;
+
+import guru.springframework.recipe.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+	@Autowired
+	UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@Test
+	void findByDescriptionTeaspoon() throws Exception{
+
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
+	}
+
+	@Test
+	void findByDescriptionCup() throws Exception{
+
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+		assertEquals("Cup", unitOfMeasureOptional.get().getDescription());
+	}
+}

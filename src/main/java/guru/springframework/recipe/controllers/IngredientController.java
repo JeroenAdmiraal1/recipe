6f851cf66f9ac7558cc21ce8ce7bd8c4fad787dc
jeroen.admiraal@alliander.com <jeroen.admiraal@alliander.com>
2021-04-09 13:44:17 +0200

    Save or update ingredients

--- src/main/java/guru/springframework/recipe/controllers/IngredientController.java
+++ src/main/java/guru/springframework/recipe/controllers/IngredientController.java
@@ -1,13 +1,17 @@
 package guru.springframework.recipe.controllers;
 
+import guru.springframework.recipe.commands.IngredientCommand;
 import guru.springframework.recipe.commands.RecipeCommand;
 import guru.springframework.recipe.services.IngredientService;
 import guru.springframework.recipe.services.RecipeService;
+import guru.springframework.recipe.services.UnitOfMeasureService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -15,11 +19,12 @@ public class IngredientController {
 
 	private final RecipeService recipeService;
 	private final IngredientService ingredientService;
+	private final UnitOfMeasureService unitOfMeasureService;
 
-
-	public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
+	public IngredientController(RecipeService recipeService, IngredientService ingredientService, UnitOfMeasureService unitOfMeasureService) {
 		this.recipeService = recipeService;
 		this.ingredientService = ingredientService;
+		this.unitOfMeasureService = unitOfMeasureService;
 	}
 
 	@GetMapping("/recipe/{id}/ingredients")
@@ -36,5 +41,22 @@ public class IngredientController {
 	public String showRecipeIngredient(@PathVariable String id, @PathVariable String ingredientId, Model model){
 		model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(id), Long.valueOf(ingredientId)));
 		return "recipe/ingredient/show";
+	}
+
+	@GetMapping("/recipe/{id}/ingredient/{ingredientId}/update")
+	public String updateRecipeIngredient(@PathVariable String id, @PathVariable String ingredientId, Model model){
+		model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(id), Long.valueOf(ingredientId)));
+		model.addAttribute("unitOfMeasureList", unitOfMeasureService.listAllUnitOfMeasure());
+		return "recipe/ingredient/ingredientform";
+	}
+
+	@PostMapping("recipe/{recipeId}/ingredient")
+	public String saveOrUpdate(@ModelAttribute IngredientCommand command){
+		IngredientCommand savedCommand = ingredientService.saveIngredientCommand(command);
+
+		log.debug("saved receipe id:" + savedCommand.getRecipeId());
+		log.debug("saved ingredient id:" + savedCommand.getId());
+
+		return "redirect:/recipe/" + savedCommand.getRecipeId() + "/ingredient/" + savedCommand.getId() + "/show";
 	}
 }
--- src/test/java/guru/springframework/recipe/controllers/IngredientControllerTest.java
+++ src/test/java/guru/springframework/recipe/controllers/IngredientControllerTest.java
@@ -4,14 +4,18 @@ import guru.springframework.recipe.commands.IngredientCommand;
 import guru.springframework.recipe.commands.RecipeCommand;
 import guru.springframework.recipe.services.IngredientService;
 import guru.springframework.recipe.services.RecipeService;
+import guru.springframework.recipe.services.UnitOfMeasureService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
@@ -27,6 +31,9 @@ class IngredientControllerTest {
 	@Mock
 	IngredientService ingredientService;
 
+	@Mock
+	UnitOfMeasureService unitOfMeasureService;
+
 	@InjectMocks
 	IngredientController ingredientController;
 
@@ -34,7 +41,7 @@ class IngredientControllerTest {
 
 	@BeforeEach
 	void setUp() {
-		ingredientController = new IngredientController(recipeService, ingredientService);
+		ingredientController = new IngredientController(recipeService, ingredientService, unitOfMeasureService);
 		mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
 	}
 
@@ -63,5 +70,34 @@ class IngredientControllerTest {
 
 		verify(ingredientService, times(1)).findByRecipeIdAndIngredientId(anyLong(), anyLong());
 
+	}
+
+	@Test
+	void testUpdateIngredientForm() throws Exception {
+		IngredientCommand ingredientCommand = new IngredientCommand();
+		when(ingredientService.findByRecipeIdAndIngredientId(anyLong(), anyLong())).thenReturn(ingredientCommand);
+		when(unitOfMeasureService.listAllUnitOfMeasure()).thenReturn(new HashSet<>());
+
+		mockMvc.perform(get("/recipe/1/ingredient/2/update"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("recipe/ingredient/ingredientform"))
+				.andExpect(model().attributeExists("ingredient"))
+				.andExpect(model().attributeExists("unitOfMeasureList"));
+	}
+
+	@Test
+	void testSaveOrUpdate() throws Exception {
+		IngredientCommand command = new IngredientCommand();
+		command.setId(3L);
+		command.setRecipeId(2L);
+
+		when(ingredientService.saveIngredientCommand(any())).thenReturn(command);
+
+		mockMvc.perform(post("/recipe/2/ingredient")
+				                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				                .param("id", "")
+				                .param("description", "somestring"))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(view().name("redirect:/recipe/2/ingredient/3/show"));
 	}
 }

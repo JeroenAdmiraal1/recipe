caace227a66df09e03988a7b78ff39707a7a3f78
jeroen.admiraal@alliander.com <jeroen.admiraal@alliander.com>
2021-04-12 16:22:54 +0200

    Add imagecontroller test

---
+++ src/test/java/guru/springframework/recipe/services/ImageServiceImplTest.java
@@ -0,0 +1,19 @@
+package guru.springframework.recipe.services;
+
+import guru.springframework.recipe.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import guru.springframework.recipe.repositories.UnitOfMeasureRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ImageServiceImplTest {
+
+	@BeforeEach
+	public void setUp() throws Exception {
+	}
+
+}

2487ac192629cf8ab3c40e308ce2b0cbf6c55dbd
jeroen.admiraal@alliander.com <jeroen.admiraal@alliander.com>
2021-05-14 15:59:59 +0200

    remove deprecated imports

--- src/test/java/guru/springframework/recipe/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -3,17 +3,11 @@ package guru.springframework.recipe.repositories;
 import guru.springframework.recipe.bootstrap.RecipeBootstrap;
 import guru.springframework.recipe.domain.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Optional;
 
